ac07a8b1ed78d3c526c9c1f214307da322a92c2d
Barsol6 <bartosz.solyga@gmail.com>
2026-05-15 12:18:21 +0200

    feat:initial commit number 2

---
+++ IOS-Diary-Project/IOS_Diary_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_Diary_ProjectApp.swift
+//  IOS-Diary-Project
+//
+//  Created by student on 15/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct IOS_Diary_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
