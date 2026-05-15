b3a1f36039b1d0f2b10c4d4c1c80f781fd4cc08a
Barsol6 <bartosz.solyga@gmail.com>
2026-05-15 12:26:31 +0200

    refactor: added core data

--- IOS-Diary-Project/IOS_Diary_ProjectApp.swift
+++ IOS-Diary-Project/IOS_Diary_ProjectApp.swift
@@ -6,12 +6,16 @@
 //
 
 import SwiftUI
+import CoreData
 
 @main
 struct IOS_Diary_ProjectApp: App {
+    let persistenceController = PersistenceController.shared
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
